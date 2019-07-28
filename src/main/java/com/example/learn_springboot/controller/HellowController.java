45b8ae8bcfc02578ef25e8b9b17abd779a6d1981
sanghun oh <sanghunoh@sanghuns-MacBook-Air.local>
2019-07-28 13:13:03 +0900

    add CrossOrigin

--- src/main/java/com/example/learn_springboot/controller/HellowController.java
+++ src/main/java/com/example/learn_springboot/controller/HellowController.java
@@ -1,5 +1,6 @@
 package com.example.learn_springboot.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ public class HellowController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+	@CrossOrigin			// allow to access others site
 	@RequestMapping(value = "/resthellow")
 	public String hellow() {		
 		return "Welcome to Hellow World !! ";
