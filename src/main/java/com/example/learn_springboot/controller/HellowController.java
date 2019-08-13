9b533f4a9c2ea2f74546f5895b269b059c330aca
sanghun oh <sanghunoh@sanghuns-MacBook-Air.local>
2019-08-13 15:55:30 +0900

    add ajax with return type

--- src/main/java/com/example/learn_springboot/controller/HellowController.java
+++ src/main/java/com/example/learn_springboot/controller/HellowController.java
@@ -1,7 +1,11 @@
 package com.example.learn_springboot.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,13 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HellowController {
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@CrossOrigin			// allow to access others site
+	@CrossOrigin // allow to access others site
 	@RequestMapping(value = "/resthellow")
-	public String hellow() {		
+	public String hellow() {
 		return "Welcome to Hellow World !! ";
+	}
+
+	@CrossOrigin // allow to access others site
+	@RequestMapping(value = "/api/ajax", method = RequestMethod.POST)
+	public String getParamWithAjax(@RequestParam Map<String, Object> paramMap) {
+		String str = (String) paramMap.get("title");
+		return "Welcome to Hellow World !! ";
+	}
+
+	@CrossOrigin // allow to access others site
+	@RequestMapping(value = "/api/ajaxReturnParamMap", method = RequestMethod.POST)
+	public Map getAndReturnParamWithAjax(@RequestParam Map<String, Object> paramMap) {
+		String str = (String) paramMap.get("title");
+		return paramMap;
 	}
 }
--- src/main/java/com/example/learn_springboot/controller/RestWSController.java
+++ src/main/java/com/example/learn_springboot/controller/RestWSController.java
@@ -28,47 +28,48 @@ public class RestWSController {
 	private ShareDataService shareDataService;
 
 	// Restful
-	@CrossOrigin			// allow to access others site
+	@CrossOrigin // allow to access others site
 	@RequestMapping(value = "/ws/{action}", method = { RequestMethod.GET, RequestMethod.POST })
 	public Object actionMethod(Map<String, Object> paramMap, @PathVariable String action) {
-		
-//		List<Object> resultObject = new ArrayList<Object>();
+
+		// List<Object> resultObject = new ArrayList<Object>();
 		Object resultObject = new Object();
 
 		// divided depending on action value
 		String viewName = "/organization/list";
-		
+
 		if ("organizationList".equalsIgnoreCase(action)) {
 			resultObject = shareDataService.getList(viewName, paramMap);
 		} else if ("organizationMap".equalsIgnoreCase(action)) {
 			resultObject = shareDataService.getObject(viewName, paramMap);
-		} 
- 
-		return resultObject;	
+		}
+
+		return resultObject;
 	}
-	
-	@CrossOrigin			// allow to access others site
+
+	@CrossOrigin // allow to access others site
 	@RequestMapping(value = "/remote/{action}", method = { RequestMethod.GET, RequestMethod.POST })
 	public Object actionRemoteMethod(Map<String, Object> paramMap, @PathVariable String action) {
-		
-//		List<Object> resultObject = new ArrayList<Object>();
+
+		// List<Object> resultObject = new ArrayList<Object>();
 		Object resultObject = new Object();
 
 		// divided depending on action value
 		String viewName = "/organization/list";
-		
+
 		RestTemplate restTemplate = new RestTemplate();
 		Map<String, String> params = new HashMap<String, String>();
 		Object tempObject = new Object();
 		String targetUri = null;
 		String keyId = null;
 
-		if ("weatherMap".equalsIgnoreCase(action)) {		// 도시 날씨
+		if ("weatherMap".equalsIgnoreCase(action)) { // 도시 날씨
 			keyId = "73989af8356ce6d4600e10678529880e";
-			targetUri = "https://api.openweathermap.org/data/2.5/weather?id=2172797&appid="+keyId;
+			targetUri = "https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=" + keyId;
 			resultObject = restTemplate.getForObject(targetUri, Object.class);
-		} else if ("addressMap".equalsIgnoreCase(action)) {	// 도로명주소 우편번호 조회서비스
-			params.put("serviceKey", "BoygPZjC27pxm92hSposjnSob2u36vziS1rzIzxkrL9QxmlhB0SMARwLfNlBE3wrE7nnw34zLmmv0a6amvW4xg%3D%3D");
+		} else if ("addressMap".equalsIgnoreCase(action)) { // 도로명주소 우편번호 조회서비스
+			params.put("serviceKey",
+					"BoygPZjC27pxm92hSposjnSob2u36vziS1rzIzxkrL9QxmlhB0SMARwLfNlBE3wrE7nnw34zLmmv0a6amvW4xg%3D%3D");
 			params.put("searchSe", "dong");
 			params.put("srchwrd", "%EC%A3%BC%EC%9B%94%EB%8F%99%20408-1");
 			params.put("currentPage", "1");
@@ -77,8 +78,8 @@ public class RestWSController {
 			targetUri = "http://openapi.epost.go.kr/postal/retrieveNewAdressAreaCdService/retrieveNewAdressAreaCdService/getNewAddressListAreaCd";
 			tempObject = restTemplate.getForObject(targetUri, Object.class, params);
 			resultObject = ((Map) tempObject).get("NewAddressListResponse");
-		} 
- 
-		return resultObject;	
+		}
+
+		return resultObject;
 	}
 }
