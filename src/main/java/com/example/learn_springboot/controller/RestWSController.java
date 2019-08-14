9dddb5b2a61addf7fb146182d9881875243e5f10
sanghun oh <sanghunoh@sanghuns-MacBook-Air.local>
2019-08-14 17:52:01 +0900

    add get and list ajax

--- src/main/java/com/example/learn_springboot/controller/RestWSController.java
+++ src/main/java/com/example/learn_springboot/controller/RestWSController.java
@@ -4,9 +4,9 @@
 package com.example.learn_springboot.controller;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
+import com.example.learn_springboot.service.OpenweatherService;
 import com.example.learn_springboot.service.ShareDataService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -26,6 +27,9 @@ public class RestWSController {
 
 	@Autowired
 	private ShareDataService shareDataService;
+
+	@Autowired
+	private OpenweatherService openweatherService;
 
 	// Restful
 	@CrossOrigin // allow to access others site
@@ -49,7 +53,7 @@ public class RestWSController {
 
 	@CrossOrigin // allow to access others site
 	@RequestMapping(value = "/remote/{action}", method = { RequestMethod.GET, RequestMethod.POST })
-	public Object actionRemoteMethod(Map<String, Object> paramMap, @PathVariable String action) {
+	public Object actionRemoteMethod(@RequestParam Map<String, Object> paramMap, @PathVariable String action) {
 
 		// List<Object> resultObject = new ArrayList<Object>();
 		Object resultObject = new Object();
@@ -58,8 +62,7 @@ public class RestWSController {
 		String viewName = "/organization/list";
 
 		RestTemplate restTemplate = new RestTemplate();
-		Map<String, String> params = new HashMap<String, String>();
-		Object tempObject = new Object();
+		Object receiveObject = new HashMap<String, String>();
 		String targetUri = null;
 		String keyId = null;
 
@@ -67,17 +70,13 @@ public class RestWSController {
 			keyId = "73989af8356ce6d4600e10678529880e";
 			targetUri = "https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=" + keyId;
 			resultObject = restTemplate.getForObject(targetUri, Object.class);
+		} else if ("weatherList".equalsIgnoreCase(action)) { // 도로명주소 우편번호 조회서비스
+			keyId = "73989af8356ce6d4600e10678529880e";
+			targetUri = "https://samples.openweathermap.org/data/2.5/history/city?q=London,UK&appid=" + keyId;
+			receiveObject = restTemplate.getForObject(targetUri, Object.class);
+			resultObject = ((Map) receiveObject).get("list");
 		} else if ("addressMap".equalsIgnoreCase(action)) { // 도로명주소 우편번호 조회서비스
-			params.put("serviceKey",
-					"BoygPZjC27pxm92hSposjnSob2u36vziS1rzIzxkrL9QxmlhB0SMARwLfNlBE3wrE7nnw34zLmmv0a6amvW4xg%3D%3D");
-			params.put("searchSe", "dong");
-			params.put("srchwrd", "%EC%A3%BC%EC%9B%94%EB%8F%99%20408-1");
-			params.put("currentPage", "1");
-			params.put("countPerPage", "10");
-
-			targetUri = "http://openapi.epost.go.kr/postal/retrieveNewAdressAreaCdService/retrieveNewAdressAreaCdService/getNewAddressListAreaCd";
-			tempObject = restTemplate.getForObject(targetUri, Object.class, params);
-			resultObject = ((Map) tempObject).get("NewAddressListResponse");
+			resultObject = openweatherService.addressMap(paramMap);
 		}
 
 		return resultObject;
---
+++ src/main/java/com/example/learn_springboot/service/OpenweatherService.java
@@ -0,0 +1,31 @@
+package com.example.learn_springboot.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class OpenweatherService {
+    public Object addressMap(Map param) {
+        RestTemplate restTemplate = new RestTemplate();
+        Map params = new HashMap<String, String>();
+        Map receiveObject = new HashMap<String, String>();
+        String targetUri = null;
+        String keyId = null;
+
+        params.put("serviceKey",
+                "BoygPZjC27pxm92hSposjnSob2u36vziS1rzIzxkrL9QxmlhB0SMARwLfNlBE3wrE7nnw34zLmmv0a6amvW4xg%3D%3D");
+        params.put("searchSe", "dong");
+        params.put("srchwrd", "주월동%20408-1");
+        params.put("currentPage", "1");
+        params.put("countPerPage", "10");
+
+        targetUri = "http://openapi.epost.go.kr/postal/retrieveNewAdressAreaCdService/retrieveNewAdressAreaCdService/getNewAddressListAreaCd";
+        receiveObject = (Map) restTemplate.getForObject(targetUri, Object.class, params);
+        Object resultObject = (Map) receiveObject.get("NewAddressListResponse");
+
+        return resultObject;
+    }
+}
